114bb1d1bd7a37380527b38add94b272062964fe
itszw <74131882+itszw@users.noreply.github.com>
2023-01-22 14:51:31 +0300

    Create Lunar.lua

---
+++ Lunar.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 8908228901 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/Lunar/main/SB2.lua",true))()
+end
+
+if game.PlaceId == 893973440 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/Lunar/main/FTF.lua",true))()
+end
