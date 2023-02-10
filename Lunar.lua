1d8f9b0719e3fd430f85aec32968230796b93cda
itszw <74131882+itszw@users.noreply.github.com>
2023-02-10 14:27:07 +0300

    Update Lunar.lua

--- Lunar.lua
+++ Lunar.lua
@@ -1,7 +1,7 @@
-if game.PlaceId == 8908228901 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/Lunar/main/SB2.lua",true))()
+if game.PlaceId == 2788229376 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/macro.lua",true))()
 end
 
-if game.PlaceId == 893973440 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/NexIsHot/Lunar/main/FTF.lua",true))()
+if game.PlaceId == 11967143103 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/macro.lua",true))()
 end
