6775addce0dfe78a86399cd1305e125de7c20d3a
itszw <74131882+itszw@users.noreply.github.com>
2023-02-13 14:14:31 +0300

    Update Lunar.lua

--- Lunar.lua
+++ Lunar.lua
@@ -2,6 +2,6 @@ if game.PlaceId == 2788229376 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/macro.lua",true))()
 end
 
-if game.PlaceId == 11967143103 then
+if game.PlaceId == 12440994551 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/macro.lua",true))()
 end
